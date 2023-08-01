1bcd3cc21b7839719f8d2bd51fa00e4e8ea73507
unknown <manirumva@gmail.com>
2023-08-01 15:16:24 +0200

    Updated script.js file

---
+++ script.js
@@ -0,0 +1,10 @@
+const hamburgerIcon = document.querySelector('.hamburger-icon');
+const menuItems = document.querySelectorAll('.menu-items');
+hamburgerIcon.addEventListener('click', () => {
+  menuItems.forEach((item) => item.classList.toggle('show'));
+  hamburgerIcon.classList.toggle('show')
+});
+document.querySelectorAll('.menu-items').forEach((item) => item.addEventListener('click', () => {
+  menuItems.forEach((item) => item.classList.remove('show'));
+  hamburgerIcon.classList.remove('show')
+}));
