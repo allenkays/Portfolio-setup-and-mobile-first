67428f9414947039d66ea97078e9ea67d287b7e2
allenkays <allenkays@proton.me>
2023-08-04 14:09:11 -0400

    Fix popup window display

--- popup.js
+++ popup.js
@@ -4,44 +4,52 @@ function hidePopup() {
   popup.style.display = 'none';
 }
 
+// Define the closeButton element outside the showPopup function
+let closeButton = document.getElementById('close-popup');
+if (!closeButton) {
+  closeButton = document.createElement('div');
+  closeButton.setAttribute('class', 'close-popup');
+  closeButton.setAttribute('id', 'close-popup');
+  closeButton.innerHTML = `
+    <span class="bar-1"></span>
+    <span class="bar-2"></span>
+  `;
+  const popupContent = document.getElementById('popup-content');
+  popupContent.appendChild(closeButton);
+  // Add event listener for the X button to hide the popup when clicked
+  closeButton.addEventListener('click', hidePopup);
+}
 // Function to show the popup with project details
 function showPopup(projectIndex) {
-  const project = projectsData[0]; // Assuming there's only one project in the data
+  const project = projectsData[0]; // Since there's only one project in the data
   const popup = document.getElementById('popup');
-  const popupContent = document.getElementById('popup-content');
-  const closeButton = document.getElementById('close-popup'); // Get the X button element
+  const popupContainer = document.querySelector('.popup-container');
+  const popupHead = document.querySelector('.popup-head');
+  const popupContent = document.querySelector('.popup-content');
+  const techStack = document.querySelector('.tech-stack');
+  const popupData = document.querySelector('.popup-data');
+  const popupLinks = document.getElementById('popup-links');
 
   // Get the relevant details for the popup based on the project index
   const name = project.name[projectIndex];
   const description1 = isDesktopScreen ? project.description[3] : project.description[2];
-  const description2 = isDesktopScreen ? project.description[4] : ('');
+  const description2 = isDesktopScreen ? project.description[4] : '';
   const technologies = [project.technologies[1], project.technologies[2], project.technologies[4]];
   const featuredImage = isDesktopScreen ? project.featuredImage[6] : project.featuredImage[5];
+  const liveLink = project.live;
+  const sourceLink = project.source;
 
-  // Create the popup content dynamically using the project details
-  popupContent.innerHTML = `
-    <div class='popup'>
-        <div class='popup-content'>
-            <div class='popup-head'>
-                <h3>${name}</h3>
-            </div>
-            <ul class='tech-stack'>
-                ${technologies.map((tech) => `<li><button class='popup-stack'>${tech}</button></li>`).join('')}
-            </ul>
-            <div class="popup-data">
-                <img src='${featuredImage}' alt='${name}'/>
-                <div class='text-links'>
-                    <p>${description1}</p>
-                    <p>${description2}</p>
-                    <div class='popup-links' id='popup-links'>
-                        <a class='popup-links' href='${project.live}' target='_blank'><button class='l-button'>See Live<img src='./assets/popups/export.svg' alt="export icon" /></button></a>
-                        <a class='popup-links' href='${project.source}' target='_blank'><button class="l-button">See Source<img src='./assets/popups/github.png' alt="github icon" /></button></a>
-                    <div>
-                </div>
-            </div>
-        </div>
-    </div>
-    `;
+  // Update the popup content dynamically using the project details
+  popupHead.querySelector('h3').textContent = name;
+  techStack.innerHTML = technologies.map((tech) => `<li><button class='popup-stack'>${tech}</button></li>`).join('');
+  popupData.querySelector('img').src = featuredImage;
+  popupData.querySelectorAll('p')[0].textContent = description1;
+  popupData.querySelectorAll('p')[1].textContent = description2;
+
+  popupLinks.innerHTML = `
+    <a class='popup-links' href='${liveLink}' target='_blank'><button class='l-button'>See Live<img src='./assets/popups/export.svg' alt="export icon" /></button></a>
+    <a class='popup-links' href='${sourceLink}' target='_blank'><button class="l-button">See Source<img src='./assets/popups/github.png' alt="github icon" /></button></a>
+  `;
 
   // Add event listener for the X button to hide the popup when clicked
   closeButton.addEventListener('click', hidePopup);
