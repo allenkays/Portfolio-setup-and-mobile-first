b86fbaa77f25ab776c0a56df1b094e43a67a97cf
allenkays <allenkays@proton.me>
2023-08-04 17:40:06 -0400

    Remove text decoration on linke buttons and fix width and display

--- script.js
+++ script.js
@@ -20,6 +20,9 @@ const projectsData = [
       'Professional Art Printing Data',
       'Data Dashboard Healthcare',
       'Website Portfolio',
+      'Professional Art Printing Data',
+      'Data Dashboard Healthcare',
+      'Website Portfolio',
     ],
     description: [
       "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
