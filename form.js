b10826bc159f542560c84c1ef784ac8f90dd8da0
allenkays <allenkays@proton.me>
2023-08-05 16:10:06 -0400

    Add client side form validation

---
+++ form.js
@@ -0,0 +1,16 @@
+// Function to validate and submit the form
+function validateAndSubmit() {
+  const emailInput = document.getElementById('email');
+  const errorMessage = document.getElementById('error-message');
+
+  // Check if the email is in lowercase
+  if (emailInput.value !== emailInput.value.toLowerCase()) {
+    errorMessage.innerText = 'Email should be in lowercase.';
+    return false; // Prevent form submission
+  }
+  errorMessage.innerText = ''; // Clear error message
+  // The form will be automatically submitted since it passed the validation.
+  return true;
+}
+
+// Add event listener below
